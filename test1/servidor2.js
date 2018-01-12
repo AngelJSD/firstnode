5b48bb5fe84f94c7acc80b6dfcbf12fe26fa3790
angel <angeljose9629@gmail.com>
2018-01-11 20:05:40 -0500

    adding some front

--- test1/servidor2.js
+++ test1/servidor2.js
@@ -2,12 +2,19 @@
 var http = require('http');
 var express = require('express');
 var mongoose = require('mongoose');
+var fs = require('fs');
 
 var mongoDB = 'mongodb://127.0.0.1/my_database';
 
 var UserModel = require('./models/usermodel');
 
+var bodyParser = require('body-parser');
 var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 mongoose.connect(mongoDB, {
   useMongoClient: true
@@ -43,9 +50,27 @@ app.get('/users', function (req, res) {
 	query.exec(function (err, users) {
   		if (err) return handleError(err);
   		res.send(users);
-	})
+	});
 });
 
+app.get('/addusers', function (req, res) {
+	/*fs.readFile("vista1.html", function(err, data){
+	  res.writeHead(200, {'Content-Type': 'text/html'});
+	  res.write(data);
+	  res.end();
+	});*/
+	res.sendFile(__dirname + '/vista1.html');
+	
+});
+
+app.post('/registerusers', function (req, res) {
+
+	//res.send();
+	console.log(req.body.name);
+	res.sendFile(__dirname + '/vista1.html');
+});
+
+//dante.cuevas@apselom.com
 /*app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
